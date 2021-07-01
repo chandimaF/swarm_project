3105b00a87a76486f99ac51577232087b4136958
mmkrusniak <mel.krusniak@yale.edu>
2021-06-30 19:10:41 -0500

    Change transmission message type for WiFi

--- src/transmit_wifi/WifiReceiver.cpp
+++ src/transmit_wifi/WifiReceiver.cpp
@@ -6,73 +6,78 @@
 #include <netinet/in.h>
 #include <string>
 #include <ros/ros.h>
-#include <std_msgs/ByteMultiArray.h>
+#include <transmit_wifi/Transmission.h>
 
 using namespace std;
 
 #define SERVER_PORT 5000
-#define CLIENT_PORT 5001
 
 int main(int argc, char ** argv) {
     ros::init(argc, argv, "wifi_receiver");
     ros::NodeHandle nodeHandle;
-    ros::Publisher pub = nodeHandle.advertise<std_msgs::ByteMultiArray>("/wifi_in", 1000);
-
-    ros::spin();
-
+    ros::Publisher pub = nodeHandle.advertise<transmit_wifi::Transmission>("/wifi_in", 1000);
 
     int server_fd, new_socket;
     struct sockaddr_in serverAddr;
-    int opt = 1;
-    char buffer[1024] = {0};
+    char buffer[1024] = {0}; // Messages are 1024 bytes long here - which is not always going to be true!
 
-    string hello = "Hello from server";
-
-    // Creating socket file descriptor
-    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Forcefully attaching socket to the port 8080
-    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                  &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+    // We don't care what our IP address is but we are definitely listening on port 5000
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(SERVER_PORT);
 
-    // Forcefully attaching socket to the port 8080
+    // We'll just transmit some chars to test
+    string hello = "bytes to transmit";
+
+    // Create the socket file descriptor
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    ROS_INFO("Got socket file descriptor");
+
     if(bind(server_fd, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
         perror("bind failed");
         ROS_WARN("Bind failed.");
         exit(EXIT_FAILURE);
     }
+    ROS_INFO("Bound socket to port 5000");
+
     if(listen(server_fd, 3) < 0) {
         perror("listen");
         ROS_WARN("Listen failed.");
         exit(EXIT_FAILURE);
     }
+    ROS_INFO("Listening on port 5000");
+
     size_t addrSize = sizeof(serverAddr);
-    if((new_socket = accept(server_fd, (struct sockaddr *) &serverAddr,
-                            (socklen_t *) &addrSize)) < 0) {
-        perror("accept");
-        ROS_WARN("Accept failed.");
-        exit(EXIT_FAILURE);
-    }
+
+    int current_client = 0;
+    bool has_client = false; // there are probably acceptable sentinals within socket fds (can they be negative?)
+                                // but not taking my chances
 
     size_t nRead;
     while(ros::ok()) {
 
-        if((nRead = read(new_socket, buffer, 1024)) > 0) {
-            std_msgs::ByteMultiArray msg = std_msgs::ByteMultiArray();
-            for(int i = 0; i < nRead; i++) {
-                msg.data[i] = buffer[i];
-            }
-            pub.publish(msg);
+        // Look for a pending client
+        if((new_socket = accept(server_fd, (struct sockaddr *) &serverAddr,
+                                (socklen_t *) &addrSize)) < 0) {
+            ROS_WARN("Could not accept a client");
+        } else {
+            has_client = true;
+            current_client = new_socket; // we kick old clients out as soon as there are new ones
+            ROS_INFO("Connected to client on a new port");
+        }
+
+        // Read off of the current client
+        if(has_client && (nRead = read(current_client, buffer, 1024)) > 0) {
             ROS_INFO("Received message via existing network");
+            transmit_wifi::Transmission msg;
+            auto bytes = vector<signed char>(buffer, buffer + nRead);
+            msg.data = bytes;
+            msg.length = nRead;
+            pub.publish(msg);
+            ROS_INFO("Published received message");
         }
     }
 }
--- src/transmit_wifi/WifiTransmitter.cpp
+++ src/transmit_wifi/WifiTransmitter.cpp
@@ -3,39 +3,21 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <string>
 #include <ros/ros.h>
-#include <std_msgs/ByteMultiArray.h>
+#include <transmit_wifi/Transmission.h>
 
 using namespace std;
 
 #define HOST "127.0.0.1"
 #define SERVER_PORT 5000
+#define CLIENT_PORT 5001
 
-void onTransmitRequested(const std_msgs::ByteMultiArray::ConstPtr & msg) {
+int sock; // I solemnly swear I will wrap this all in a class at the next possible refactoring opportunity
+
+void onTransmitRequested(const transmit_wifi::Transmission::ConstPtr & msg) {
+    size_t nbytes = msg->data.size();
     const signed char * bytes = msg.get()->data.data();
-
-    int sock;
-    struct sockaddr_in serverAddr;
-    string hello = (char *) bytes; // this will definitely not cause issues later
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        ROS_WARN("Socket creation error.");return;
-    }
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(SERVER_PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, HOST, &serverAddr.sin_addr) <= 0) {
-        ROS_WARN("Invalid address."); return;
-    }
-
-    if (connect(sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
-        ROS_WARN("\nConnection Failed"); return;
-    }
-
-    send(sock, hello.c_str(), hello.size(), 0);
+    send(sock, bytes, nbytes, 0);
     ROS_INFO("Sent message via existing network");
 }
 
@@ -43,7 +25,40 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "wifi_transmitter");
     ros::NodeHandle nodeHandle;
+
+    // Transmitter is the client. (only two devices supported though.)
+    struct sockaddr_in serverAddr, clientAddr;
+
+    // Get a socket file pointer
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        ROS_WARN("Socket creation error."); return 1;
+    }
+    ROS_INFO("Got socket file descriptor");
+
+
+    // Note how we don't bind() here - we don't really care what the transmitter (outgoing) port is.
+    // We do care what the server port is, because it'll be listening on 5000.
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, HOST, &serverAddr.sin_addr) <= 0) {
+        ROS_WARN("Invalid address."); return 1;
+    }
+
+    ROS_INFO("Attempting connection to server...");
+    // Connect socket to server!
+    if (connect(sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Connection failed");
+        ROS_WARN("\nConnection Failed"); return 1;
+    }
+    ROS_INFO("Socket connected to server");
+
+    // We want to transmit out of that socket into /wifi_out as needed.
     ros::Subscriber sub = nodeHandle.subscribe("/wifi_out", 100, onTransmitRequested);
+
+    // I don't *think* this socket getting closed spontaneously will be a problem. But it might be better to set
+    //   SO_KEEPALIVE? We'll see.
 
     ros::spin();
 }
